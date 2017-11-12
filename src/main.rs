464f473701ab8dee99215980bcc4ebe6760c1744
Peter Higley <phigley@gmail.com>
2017-11-12 14:07:34 -0800

    Experiment with creating an entrance and exit

--- src/main.rs
+++ src/main.rs
@@ -16,25 +16,61 @@ fn main() {
 
 fn execute() -> Result<(), String> {
     let potentials = [
-        // CharacterEntry::build('─', 10.0, "000|101|000")?,
-        // CharacterEntry::build('│', 10.0, "010|000|010")?,
-        // CharacterEntry::build('┌', 1.0, "000|001|010")?,
-        // CharacterEntry::build('┐', 1.0, "000|100|010")?,
-        // CharacterEntry::build('└', 1.0, "010|001|000")?,
-        // CharacterEntry::build('┘', 1.0, "010|100|000")?,
+        CharacterEntry::build('─', 10.0, "000|101|000")?,
+        CharacterEntry::build('│', 10.0, "010|000|010")?,
+        CharacterEntry::build('┌', 1.0, "000|001|010")?,
+        CharacterEntry::build('┐', 1.0, "000|100|010")?,
+        CharacterEntry::build('└', 1.0, "010|001|000")?,
+        CharacterEntry::build('┘', 1.0, "010|100|000")?,
         // Notice that adding the ' ' character causes many failures!
         // Not re-assuring for this technique to be used as a decent
         // constraint solution.
-        CharacterEntry::build(' ', 1.0, "000|000|000")?,
-        CharacterEntry::build('╱', 1.0, "001|000|100")?,
-        CharacterEntry::build('╲', 1.0, "100|000|001")?,
-        CharacterEntry::build('╳', 1.0, "101|000|101")?,
+        // CharacterEntry::build(' ', 1.0, "000|000|000")?,
+        // CharacterEntry::build('╱', 1.0, "001|000|100")?,
+        // CharacterEntry::build('╲', 1.0, "100|000|001")?,
+        // CharacterEntry::build('╳', 1.0, "101|000|101")?,
     ];
 
     let mut field = Field::new(&potentials, 80, 40);
 
-    if true || field.close_edges() {
+    if field.close_edges() {
         let mut rng = rand::thread_rng();
+
+        // Building an entrance requires the square pieces and space.
+        // If the space is missing, it will fail to force the potentials
+        if false {
+            // Set the entrance as 2 horizontal lines in the upper left.
+            if !field.force_potential(0, 5, 5) {
+                println!(
+                    "{}",
+                    entry::make_string(&potentials, &field.render_partial())
+                );
+                return Err(String::from("Failed to create entrance at (0,5)"));
+            }
+            if !field.force_potential(0, 6, 3) {
+                println!(
+                    "{}",
+                    entry::make_string(&potentials, &field.render_partial())
+                );
+                return Err(String::from("Failed to create entrance at (0,6)"));
+            }
+
+            // Set the exit as 2 horizontal lines in the lower right.
+            if !field.force_potential(79, 35, 4) {
+                println!(
+                    "{}",
+                    entry::make_string(&potentials, &field.render_partial())
+                );
+                return Err(String::from("Failed to create entrance at (79,35)"));
+            }
+            if !field.force_potential(79, 36, 2) {
+                println!(
+                    "{}",
+                    entry::make_string(&potentials, &field.render_partial())
+                );
+                return Err(String::from("Failed to create entrance at (79,36)"));
+            }
+        }
 
         for i in 0..20 {
             match run_field(&potentials, field.clone(), &mut rng) {
